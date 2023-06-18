74e6f11f36f8ee7b746ccdd1e28a4bd6d67861fb
Jakobi Haskell <jakobi_haskell@brown.edu>
2023-06-18 02:27:31 -0400

    in middle of tables

--- zigzag_finance/src/App.tsx
+++ zigzag_finance/src/App.tsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
-import './styles/index.css'
+import "./styles/index.css";
 import LaunchApp from "./LaunchApp";
 import FundTable from "./FundTable";
 // Default styles that can be overridden by your app
 // require("@solana/wallet-adapter-react-ui/styles.css");
 
 const Home = () => {
-
   const [launchApp, setLaunchApp] = useState(false);
 
   const handleLaunchApp = () => {
@@ -18,26 +17,36 @@ const Home = () => {
   return (
     <div className="bg-black w-screen h-screen box-border">
       <div className="w-screen flex justify-end">
-          { !launchApp ? 
-          <div className={`mr-2 mt-2 nav-link py-5 px-4 bg-gradient-to-br from-pink-600 to-purple-500 text-white rounded-xl font-bold hover:-translate-y-1  transition-all ${launchApp ? 'invisible': 'visible'}`} onClick={handleLaunchApp}>
+        {!launchApp ? (
+          <div
+            className={`mr-2 mt-2 nav-link py-5 px-4 bg-gradient-to-br from-pink-600 to-purple-500 text-white rounded-xl font-bold hover:-translate-y-1  transition-all ${
+              launchApp ? "invisible" : "visible"
+            }`}
+            onClick={handleLaunchApp}
+          >
             Launch App
           </div>
+        ) : null}
+      </div>
 
-          : null }
+      {launchApp ? (
+        <FundTable />
+      ) : (
+        <>
+          <div className="container">
+            <div className="content w-screen mt-40 flex flex-col justify-center items-center gap-4">
+              <img src="ZizZagFin.png" className="w-24 mb-3"></img>
+              <h1 className="text-white text-6xl text-center font-extrabold italic">
+                ZigZag Finance
+              </h1>
+              <h2 className="text-pink-600 font-semibold text-2xl">
+                On-Chain Finance Management
+              </h2>
+            </div>
           </div>
-         
-      { launchApp ? <LaunchApp /> : 
-      <><div className="container">
-          <div className="content w-screen mt-40 flex flex-col justify-center items-center gap-4">
-            <img src='ZizZagFin.png' className="w-24 mb-3"></img>
-            <h1 className="text-white text-6xl text-center font-extrabold italic">ZigZag Finance</h1>
-            <h2 className="text-pink-600 font-semibold text-2xl">On-Chain Finance Management</h2>
-          </div>
-        </div><FundTable></FundTable></>
-      }
-
+        </>
+      )}
     </div>
-
   );
 };
 
--- zigzag_finance/src/FundTable.tsx
+++ zigzag_finance/src/FundTable.tsx
@@ -44,23 +44,23 @@ const FundTable = () => {
   return (
     <>
       {/* table */}
-      <div className="container">
-        <table className="table">
-          <tr>
-            <th>Fund Name</th>
-            <th>AUM</th>
-            <th>This Month</th>
-            <th>24 hr</th>
-            <th>Last 7 days</th>
+      <div className="h-screen w-screen flex justify-center items-center">
+        <table className="table py-4 px-2 text-white">
+          <tr className=" bg-gray-800 component py-4 px-2 text-gray-400 text-right">
+            <th className="px-4 py-2">Fund Name</th>
+            <th className="px-4 py-2">AUM</th>
+            <th className="px-4 py-2">This Month</th>
+            <th className="px-4 py-2">24 hr</th>
+            <th className="px-4 py-2">Last 7 days</th>
           </tr>
 
           {tableentries.map((entry, index) => (
-            <tr id={index.toString()}>
-              <td className="fund-name">{entry.fundname}</td>
-              <td className="aum">{entry.aum}</td>
-              <td className="this-month">{entry.thismonth}</td>
-              <td className="24-hrs">{entry.twentyfourhr}</td>
-              <td className="buy-sell">
+            <tr id={index.toString()} className={` bg-gray-700 component py-4 px-2`}>
+              <td className="fund-name px-4 py-2">{entry.fundname}</td>
+              <td className="aum px-4 py-2">{entry.aum}</td>
+              <td className="this-month px-4 py-2">{entry.thismonth}</td>
+              <td className="24-hrs px-4 py-2">{entry.twentyfourhr}</td>
+              <td className="buy-sell px-4 py-2">
                 <BuySell />
               </td>
             </tr>
