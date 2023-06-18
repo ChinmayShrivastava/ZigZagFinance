b1c9f2fea5a7258b9b9e5d4852db98bea7e6118b
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2023-06-18 03:43:50 -0400

    Update UserForm.tsx

--- zigzag_finance/src/UserForm.tsx
+++ zigzag_finance/src/UserForm.tsx
@@ -18,40 +18,46 @@ const UserForm = () => {
         });
     }
 
+    const onSubmit = (e: any) => {
+        e.preventDefault();
+        console.log(form);
+    }
+
     return (
-        <div>
-            <form>
-                <table>
-                    <tr>
-                        <td>
+        <div className="flex w-screen justify-center flex-column align-center h-screen items-center">
+            <form className="bg-gray-800 component text-gray-400 text-right" onSubmit={onSubmit}>
+                <table className="table py-4 px-2 text-white shadow-2xl">
+                    <tr className='bg-gray-800 component py-6 px-2 text-gray-400 text-right'>
+                        <th className="px-6 py-6">Management Information</th>
+                    </tr>
+                    <tr className={`bg-gray-700 component py-4 px-6 hover:bg-gray-600 hover:cursor-pointer font-light`} style={{borderRadius: "10px"}}>
+                        <td className="px-6 py-6 flex justify-left">
                             <label htmlFor="vaultname">Vault Name</label>
                         </td>
-                        <td>
-                            <input type="text" name="vaultname" placeholder='MNY' onChange={handleChange} />
+                        <td className="px-6 py-6">
+                            <input className='bg-transparent placeholder:opacity-60 w-full h-4/6 focus:outline-none placeholder:pl-2' type="text" name="vaultname" placeholder='MNY' onChange={handleChange} />
                         </td>
                     </tr>
-                    <tr>
-                        <td>
-                            <label htmlFor="denominationasset">Denomination Asset</label>
-                        </td>
-                        <td>
-                            <input type="text" name="denominationasset" placeholder='DAI' onChange={handleChange} />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
+                    <tr className={`bg-gray-700 component py-4 px-6 hover:bg-gray-600 hover:cursor-pointer font-light`} style={{borderRadius: "10px"}}>
+                        <td className="px-6 py-6 flex justify-left">
                             <label htmlFor="managementfee">Management Fee</label>
                         </td>
-                        <td>
-                            <input type="text" name="managementfee" placeholder='--.--%' onChange={handleChange} />
+                        <td className="px-6 py-6">
+                            <input className='bg-transparent placeholder:opacity-60 w-full h-4/6 focus:outline-none placeholder:pl-2' type="text" name="managementfee" placeholder='--.--%' onChange={handleChange} />
                         </td>
                     </tr>
-                    <tr>
-                        <td>
+                    <tr className={`bg-gray-700 component py-4 px-6 hover:bg-gray-600 hover:cursor-pointer font-light`} style={{borderRadius: "10px"}}>
+                        <td className="px-6 py-6 flex justify-left">
                             <label htmlFor="exitfee">Exit Fee</label>
                         </td>
-                        <td>
-                            <input type="text" name="exitfee" placeholder='--.--%' onChange={handleChange} />
+                        <td className="px-6 py-6">
+                            <input className='bg-transparent placeholder:opacity-60 w-full h-4/6 focus:outline-none placeholder:pl-2' type="text" name="exitfee" placeholder='--.--%' onChange={handleChange} />
+                        </td>
+                    </tr>
+                    <tr className={`bg-gray-700 component py-4 px-6 font-light`}>
+                        <td></td>
+                        <td className="px-6 py-6 flex">
+                            <button type='submit' className="buy-button bg-gray-400 rounded-2xl py-3 px-5 justify-self-end">Create</button>
                         </td>
                     </tr>
                 </table>
