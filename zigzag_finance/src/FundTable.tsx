415fc653a4c5c69f3512583db8e9034d16efc8c7
Jakobi Haskell <jakobi_haskell@brown.edu>
2023-06-18 02:59:38 -0400

    in midddle of table and modal changes

--- zigzag_finance/src/FundTable.tsx
+++ zigzag_finance/src/FundTable.tsx
@@ -34,8 +34,12 @@ const tableentries = [
 const BuySell = () => {
   return (
     <>
-      <button className="buy-button">Buy</button>
-      <button className="sell-button">Sell</button>
+      <td>
+        <button className="buy-button bg-gray-400 rounded-2xl py-3">Buy</button>
+      </td>
+      <td>
+        <button className="sell-button">Sell</button>
+      </td>
     </>
   );
 };
@@ -44,24 +48,37 @@ const FundTable = () => {
   return (
     <>
       {/* table */}
-      <div className="h-screen w-screen flex justify-center items-center">
-        <table className="table py-4 px-2 text-white">
-          <tr className=" bg-gray-800 component py-4 px-2 text-gray-400 text-right">
-            <th className="px-4 py-2">Fund Name</th>
-            <th className="px-4 py-2">AUM</th>
-            <th className="px-4 py-2">This Month</th>
-            <th className="px-4 py-2">24 hr</th>
-            <th className="px-4 py-2">Last 7 days</th>
+      <div className="h-screen w-screen flex justify-center items-center bg-gray-900">
+        <table className="table py-4 px-2 text-white shadow-2xl rounded-lg">
+          <tr className=" bg-gray-800 component py-6 px-2 text-gray-400 text-right">
+            <th className="px-6 py-6">Fund Name</th>
+            <th className="px-6 py-6">AUM</th>
+            <th className="px-6 py-6">30d</th>
+            <th className="px-6 py-6">24hr</th>
+            <th className="px-6 py-6"></th>
+            <th></th>
+
           </tr>
 
           {tableentries.map((entry, index) => (
-            <tr id={index.toString()} className={` bg-gray-700 component py-4 px-2`}>
-              <td className="fund-name px-4 py-2">{entry.fundname}</td>
-              <td className="aum px-4 py-2">{entry.aum}</td>
-              <td className="this-month px-4 py-2">{entry.thismonth}</td>
-              <td className="24-hrs px-4 py-2">{entry.twentyfourhr}</td>
-              <td className="buy-sell px-4 py-2">
-                <BuySell />
+            <tr
+              id={index.toString()}
+              className={` bg-gray-700 component py-4 px-6 hover:bg-gray-600 hover:cursor-pointer font-light rounded-lg`}
+              style={{
+                borderRadius: "10px",
+              }}
+            >
+              <td className="fund-name px-6 py-6">{entry.fundname}</td>
+              <td className="aum px-6 py-6">{entry.aum}</td>
+              <td className="this-month px-6 py-6">{entry.thismonth}</td>
+              <td className="24-hrs px-6 py-6">{entry.twentyfourhr}</td>
+              <td className="buy-sell px-6 py-6 flex gap-2">
+                {/* <BuySell /> */}
+                <button className="buy-button bg-blue-800 rounded-xl py-3 px-6 hover:bg-blue-700 transition-all">
+                  Buy
+                </button>
+              
+                <button className="sell-button bg-orange-700 hover:bg-orange-600 transition-all rounded-xl py-3 px-6">Sell</button>
               </td>
             </tr>
           ))}
